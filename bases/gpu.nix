417d712232a5cdef5ce16b720ae31bd1ee4f547b
KongJianGhost <2501491361@qq.com>
2025-10-09 13:21:56 +0800

    fix:intel gpu backlight

--- bases/gpu.nix
+++ bases/gpu.nix
@@ -9,7 +9,9 @@
     enable = true;
   };
   services.xserver.videoDrivers = [ "nvidia" "modesetting"];
+  boot.kernelParams = [ "acpi_backlight=native" ];
   environment.systemPackages = with pkgs; [
+    vpl-gpu-rt
     (writeShellScriptBin "nvidia-offload" ''
       export __NV_PRIME_RENDER_OFFLOAD=1
       export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
