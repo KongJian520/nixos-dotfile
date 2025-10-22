1040140f525358a5dacb06accbae20f65abbcf40
KongJianGhost <2501491361@qq.com>
2025-10-22 15:02:30 +0800

    update

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,5 +1,9 @@
 { config, pkgs, ... }:
+let
+  nvimPath = "${config.home.homeDirectory}/.nixos/home/neovim/.config";
+in
 {
+  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink nvimPath;
   programs.neovim = {
     enable = true;
     defaultEditor = true;
