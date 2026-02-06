c8c832c7147e6d243b3534daff318398ba91ee11
KongJianGhost <2501491361@qq.com>
2026-02-06 16:30:39 +0800

    add niri

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -6,6 +6,14 @@
   ...
 }:
 {
+  programs.swaylock = {
+    enable = true;
+    settings = {
+      color = "000000"; # 背景颜色设为黑色
+      show-failed-attempts = true; # 显示失败次数
+      ring-color = "ffff00"; # 锁定环的颜色
+    };
+  };
   programs.niri.enable = true;
   home.file.".config/niri/" = {
     source = ./config;
